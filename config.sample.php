a11fdaeed2b7525f6d90cbe42c4997ba0736a31a
Julie Pelletier <blondie101010@hotmail.com>
2020-10-13 18:07:31 -0400

    initial commit

---
+++ config.sample.php
@@ -0,0 +1,11 @@
+<?php
+
+$config = [
+    'input' => 'inputQueue',
+    'outputs' => [
+        ['type' => 'queue', 'outputQueue1'],
+        ['type' => 'queue', 'outputQueue2'],
+        ['type' => 'file', 'outputFile1'],
+        ['type' => 'file', 'outputFile2']
+    ],
+];
